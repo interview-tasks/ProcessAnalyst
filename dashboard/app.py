0e0ba023087b0d2c7a2721d1c71a4f63904bed0c
Ismat <ismetsemedov@gmail.com>
2025-03-27 22:20:01 +0400

    render

---
+++ dashboard/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='dashboard')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
